ba012eccd1cc62a441e2e6eb372fa6b89daeca6a
Cphayim <i@cphayim.me>
2025-04-29 03:35:58 +0800

    Adds integration tests to the workflow

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,4 @@
 import app from 'lib/createServer'
 
-app.listen(3000, () => console.log(`🚀 Server ready at: http://localhost:3000`))
+// Starts up the app
+app.listen(3000, () => console.log(`🚀 Server ready at: http://127.0.0.1:3000`))
